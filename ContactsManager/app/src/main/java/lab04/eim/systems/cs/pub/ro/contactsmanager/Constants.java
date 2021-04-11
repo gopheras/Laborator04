8d4db836a8278625a90440ae04cd91ec7409653f
gopheras <razvan.stoica0211@stud.acs.upb.ro>
2021-04-11 23:34:25 +0300

    implemented tasks for laboratory 04

---
+++ ContactsManager/app/src/main/java/lab04/eim/systems/cs/pub/ro/contactsmanager/Constants.java
@@ -0,0 +1,5 @@
+package lab04.eim.systems.cs.pub.ro.contactsmanager;
+
+public interface Constants {
+    final public static int CONTACTS_MANAGER_REQUEST_CODE = 2017;
+}
---
+++ ContactsManager/app/src/main/java/lab04/eim/systems/cs/pub/ro/contactsmanager/ContactsManagerActivity.java
@@ -0,0 +1,155 @@
+package lab04.eim.systems.cs.pub.ro.contactsmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ContactsManagerActivity extends AppCompatActivity {
+
+    private EditText nameEditText;
+    private EditText phoneEditText;
+    private EditText emailEditText;
+    private EditText addressEditText;
+    private EditText jobTitleEditText;
+    private EditText companyEditText;
+    private EditText websiteEditText;
+    private EditText imEditText;
+
+    private Button toggleAdditional;
+    private Button saveData;
+    private Button cancelData;
+    private LinearLayout additionalFieldsContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts_manager);
+
+        nameEditText = (EditText)findViewById(R.id.name_edit_text);
+        phoneEditText = (EditText)findViewById(R.id.phone_number_edit_text);
+        emailEditText = (EditText)findViewById(R.id.email_edit_text);
+        addressEditText = (EditText)findViewById(R.id.address_edit_text);
+        jobTitleEditText = (EditText)findViewById(R.id.job_title_edit_text);
+        companyEditText = (EditText)findViewById(R.id.company_edit_text);
+        websiteEditText = (EditText)findViewById(R.id.website_edit_text);
+        imEditText = (EditText)findViewById(R.id.im_edit_text);
+
+        additionalFieldsContainer = (LinearLayout)findViewById(R.id.additional_fields_layout);
+
+        toggleAdditional = (Button)findViewById(R.id.buttonToggle);
+        toggleAdditional.setOnClickListener(buttonClickListener);
+        saveData = (Button)findViewById(R.id.buttonSave);
+        saveData.setOnClickListener(buttonClickListener);
+        cancelData = (Button)findViewById(R.id.buttonClear);
+        cancelData.setOnClickListener(buttonClickListener);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String phone = intent.getStringExtra("lab04.eim.systems.cs.pub.ro.contactsmanager.PHONE_NUMBER_KEY");
+            if (phone != null) {
+                phoneEditText.setText(phone);
+            } else {
+                Toast.makeText(this, getResources().getString(R.string.phone_error), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.buttonToggle:
+                    switch (additionalFieldsContainer.getVisibility()) {
+                        case View.VISIBLE:
+                            toggleAdditional.setText(getResources().getString(R.string.show_fields));
+                            additionalFieldsContainer.setVisibility(View.INVISIBLE);
+                            break;
+                        case View.INVISIBLE:
+                            toggleAdditional.setText(getResources().getString(R.string.hide_fields));
+                            additionalFieldsContainer.setVisibility(View.VISIBLE);
+                            break;
+                    }
+                    break;
+
+                case R.id.buttonSave:
+                    String name = nameEditText.getText().toString();
+                    String phone = phoneEditText.getText().toString();
+                    String email = emailEditText.getText().toString();
+                    String address = addressEditText.getText().toString();
+                    String jobTitle = jobTitleEditText.getText().toString();
+                    String company = companyEditText.getText().toString();
+                    String website = websiteEditText.getText().toString();
+                    String im = imEditText.getText().toString();
+
+                    Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+                    intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+
+                    if (name != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.NAME, name);
+                    }
+                    if (phone != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.PHONE, phone);
+                    }
+                    if (email != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.EMAIL, email);
+                    }
+                    if (address != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.POSTAL, address);
+                    }
+                    if (jobTitle != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.JOB_TITLE, jobTitle);
+                    }
+                    if (company != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.COMPANY, company);
+                    }
+                    ArrayList<ContentValues> contactData = new ArrayList<ContentValues>();
+                    if (website != null) {
+                        ContentValues websiteRow = new ContentValues();
+                        websiteRow.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Website.CONTENT_ITEM_TYPE);
+                        websiteRow.put(ContactsContract.CommonDataKinds.Website.URL, website);
+                        contactData.add(websiteRow);
+                    }
+                    if (im != null) {
+                        ContentValues imRow = new ContentValues();
+                        imRow.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE);
+                        imRow.put(ContactsContract.CommonDataKinds.Im.DATA, im);
+                        contactData.add(imRow);
+                    }
+
+                    intent.putParcelableArrayListExtra(ContactsContract.Intents.Insert.DATA, contactData);
+                    startActivity(intent);
+                    break;
+
+                case R.id.buttonClear:
+                    setResult(Activity.RESULT_CANCELED, new Intent());
+                    finish();
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        switch (requestCode) {
+            case Constants.CONTACTS_MANAGER_REQUEST_CODE:
+                setResult(resultCode, new Intent());
+                finish();
+                break;
+        }
+    }
+}
